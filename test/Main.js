dc6c040b95fa77b12d106fd647e1d9ff05882e1b
Phil Freeman <paf31@cantab.net>
2017-04-21 11:53:20 -0700

    Add a better example

--- test/Main.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-exports.display = function (s) {
-  return function() {
-    document.body.innerText = s;
-  };
-};
--- test/Main.purs
+++ test/Main.purs
@@ -1,49 +1,81 @@
 module Test.Main where
 
 import Prelude
-import FRP.Behavior.Keyboard as Keyboard
 import FRP.Behavior.Mouse as Mouse
 import FRP.Behavior.Time as Time
+import Color (black, lighten, white)
 import Control.Monad.Eff (Eff)
-import Data.Array (fromFoldable, length)
-import Data.Foldable (fold)
+import Data.Array ((..))
+import Data.Foldable (foldMap)
 import Data.Int (toNumber)
-import Data.Maybe (maybe)
+import Data.Maybe (fromJust, maybe)
+import Data.Traversable (sequence)
 import Data.Tuple (Tuple(..))
 import FRP (FRP)
-import FRP.Behavior (Behavior, sample_, derivative, integral)
+import FRP.Behavior (Behavior, derivative, sample_)
 import FRP.Event (subscribe)
 import FRP.Event.Time (interval)
+import Global (infinity)
+import Graphics.Canvas (CANVAS, getCanvasElementById, getCanvasHeight, getCanvasWidth, getContext2D, setCanvasHeight, setCanvasWidth)
+import Graphics.Drawing (Drawing, circle, fillColor, filled, lineWidth, outlineColor, outlined, rectangle, render, scale, translate)
+import Partial.Unsafe (unsafePartial)
 
-foreign import display :: forall eff. String -> Eff eff Unit
+type Circle = { x :: Number, y :: Number, size :: Number }
 
-mouse :: Behavior (Tuple Int Int)
-mouse = maybe (Tuple 0 0) (\{ x, y } -> Tuple x y) <$> Mouse.position
+scene :: { w :: Number, h :: Number } -> Behavior Drawing
+scene { w, h } = pure background <> map renderCircles circles where
+  background :: Drawing
+  background = filled (fillColor white) (rectangle 0.0 0.0 w h)
 
-all :: Behavior String
-all = fold
-  [ pure "millisSinceEpoch: "
-  , map show Time.millisSinceEpoch
-  , pure "\nmouse: "
-  , map show mouse
-  , pure "\nbuttons: "
-  , map (show <<< fromFoldable) Mouse.buttons
-  , pure "\nkeys: "
-  , map (show <<< fromFoldable) Keyboard.keys
-  , pure "\nintegral: "
-  , map show (integral' buttonCount)
-  , pure "\nderivative: "
-  , map show (derivative div2 (map ((_ / 1000.0) <<< toNumber) Time.millisSinceEpoch) (map (\(Tuple x y) -> Tuple (toNumber x) (toNumber y)) mouse))
-  ]
+  scaleFactor :: Number
+  scaleFactor = max w h / 10.0
 
-div2 :: Tuple Number Number -> Number -> Tuple Number Number
-div2 (Tuple x y) z = Tuple (x / z) (y / z)
+  renderCircle :: Circle -> Drawing
+  renderCircle { x, y, size } =
+    scale scaleFactor scaleFactor <<< translate x y <<< scale size size $
+      outlined
+        (outlineColor (lighten (0.8 - size * 0.2) black) <> lineWidth ((1.0 + size * 2.0) / scaleFactor))
+        (circle 0.0 0.0 0.5)
 
-buttonCount :: Behavior Number
-buttonCount = map (toNumber <<< length <<< fromFoldable) Mouse.buttons
+  renderCircles :: Array Circle -> Drawing
+  renderCircles = foldMap renderCircle
 
-integral' :: Behavior Number -> Behavior Number
-integral' = integral mul (map ((_ / 1000.0) <<< toNumber) Time.millisSinceEpoch)
+  mouse :: Behavior (Tuple Number Number)
+  mouse = maybe (Tuple 0.0 0.0) (\{ x, y } -> Tuple (toNumber x) (toNumber y)) <$> Mouse.position
 
-main :: forall eff. Eff (frp :: FRP | eff) Unit
-main = sample_ all (interval 50) `subscribe` display
+  seconds :: Behavior Number
+  seconds = map ((_ / 1000.0) <<< toNumber) Time.millisSinceEpoch
+
+  speed :: Behavior Number
+  speed =
+    map (\(Tuple dx dy) -> (dx * dx + dy * dy) / scaleFactor / scaleFactor) $
+      derivative
+        (\(Tuple x y) z -> Tuple (x / z) (y / z))
+        seconds
+        mouse
+
+  circles :: Behavior (Array Circle)
+  circles = sequence do
+    i <- 0 .. 10
+    j <- 0 .. 10
+    let x = toNumber i
+        y = toNumber j
+        toCircle (Tuple mx my) s =
+          let dx = x - mx / scaleFactor
+              dy = y - my / scaleFactor
+          in { x
+             , y
+             , size: 0.5 + (1.0 + s / 200.0) / (dx * dx + dy * dy + 1.5)
+             }
+    pure (toCircle <$> mouse <*> speed)
+
+main :: forall eff. Eff (canvas :: CANVAS, frp :: FRP | eff) Unit
+main = do
+  mcanvas <- getCanvasElementById "canvas"
+  let canvas = unsafePartial (fromJust mcanvas)
+  ctx <- getContext2D canvas
+  w <- getCanvasWidth canvas
+  h <- getCanvasHeight canvas
+  _ <- setCanvasWidth w canvas
+  _ <- setCanvasHeight h canvas
+  sample_ (scene { w, h }) (interval 80) `subscribe` render ctx
