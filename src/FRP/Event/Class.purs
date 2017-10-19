020be907b820ae4d47b1a79a45157b55ef090354
Tom <tomjharding@live.co.uk>
2017-10-19 20:01:51 +0100

    Fix comments from review
    Put the functions in the wrong place. Oops. This commit moves the
    `gate`-related functions from `FRP.Event.Time` to `FRP.Event.Class`, and
    exports them.

--- src/FRP/Event/Class.purs
+++ src/FRP/Event/Class.purs
@@ -9,13 +9,15 @@ module FRP.Event.Class
   , sampleOn_
   , keepLatest
   , fix
+  , gate
+  , gateBy
   , module Data.Filterable
   ) where
 
 import Prelude
 
 import Control.Alternative (class Alternative)
-import Data.Filterable (class Filterable, filterMap)
+import Data.Filterable (class Filterable, filterMap, filtered)
 import Data.Maybe (Maybe(..))
 import Data.Monoid (class Monoid, mempty)
 import Data.Tuple (Tuple(..), snd)
@@ -67,3 +69,21 @@ mapAccum f xs acc = filterMap snd
 -- | the second event.
 sampleOn_ :: forall event a b. IsEvent event => event a -> event b -> event a
 sampleOn_ a b = sampleOn a (b $> id)
+
+-- | Sample the events that are fired while a boolean event is true.
+gate :: forall a event. IsEvent event => event Boolean -> event a -> event a
+gate = gateBy const
+
+-- | Generalised form of `gateBy`, allowing for any predicate between
+-- | the two events. When true, the second event is sampled.
+gateBy
+  :: forall a b event.
+     IsEvent event
+  => (a -> b -> Boolean)
+  -> event a
+  -> event b
+  -> event b
+gateBy f s
+   = filtered
+ <<< sampleOn s
+ <<< map \x p -> if f p x then Just x else Nothing
